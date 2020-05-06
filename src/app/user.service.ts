a993a7a4102f58fd52f87a54f37cb3f4acf380bc
Ajit Gupta <57554561+devajitgupta@users.noreply.github.com>
2020-05-06 11:04:06 +0530

    ajit
    add model

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-
+import { Observable } from 'rxjs';
+import {model_name} from 'path';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private ROOT_URL = "http://localhost/api/";
+  
+  constructor(private http: HttpClient){}
+  
+    getData(): Observable<Data[]> {
+    return this.http.get<Data[]>(this.ROOT_URL);
+
+  
 
 }
